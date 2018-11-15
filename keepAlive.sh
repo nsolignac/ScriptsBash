cba18489163afb8c93335035e0b6702efa906ec3
SpaceHotDog <nicolassolignac@gmail.com>
2018-11-15 17:39:24 -0300

    Print del CurrentFocus Window

--- keepAlive.sh
+++ keepAlive.sh
@@ -10,4 +10,13 @@ read DEVICE
 echo "Indique el nombre de la app: "
 read APP
 
+# Lista los paquetes por nombre
 adb -s $DEVICE shell pm list packages -f | grep -i $APP
+
+# Lista nombre del paquete y actividad actual,
+# de la app que se encuentra abierta actualmente
+# en el equipo
+
+adb -s $DEVICE shell dumpsys window windows | grep -E 'mCurrentFocus'
+
+# 

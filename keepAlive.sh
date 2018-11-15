d52b841ecbdf7ce2cbfcf542641c30fe55693c75
SpaceHotDog <nicolassolignac@gmail.com>
2018-11-15 17:54:39 -0300

    Control de activities

--- keepAlive.sh
+++ keepAlive.sh
@@ -7,6 +7,7 @@ echo "Defaults ID's : emulator-5554 (Tandem)"
 echo "Defaults ID's : emulator-5556 (Argenpesos)"
 read DEVICE
 
+# Mapea variable con nombre de la app
 echo "Indique el nombre de la app: "
 read APP
 
@@ -16,7 +17,10 @@ adb -s $DEVICE shell pm list packages -f | grep -i $APP
 # Lista nombre del paquete y actividad actual,
 # de la app que se encuentra abierta actualmente
 # en el equipo
-
 adb -s $DEVICE shell dumpsys window windows | grep -E 'mCurrentFocus'
+# Eg. "WhatsApp" - mCurrentFocus=Window{c371bb4 u0 com.whatsapp/com.whatsapp.HomeActivity}
+# En el caso anterior, {package.name}="com.whatsapp"
+# {com.package.name.ActivityName}="com.whatsapp.HomeActivity"
 
-# 
+# Ejecuta Activity de una app
+am start -n com.package.name/com.package.name.ActivityName

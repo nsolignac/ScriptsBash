cec335488758bfc9b8276f04fcdca2a220a53a44
SpaceHotDog <nicolassolignac@gmail.com>
2018-11-20 12:17:50 -0300

    var's, reordenamiento y se sacan logs de segunda etapa

---
+++ keepAliveAndroid.sh
@@ -0,0 +1,19 @@
+# Se guarda el valor de $DEVICE
+echo "Indique el dispositivo incluyendo guion y numero: "
+read DEVICE
+
+# Directorio donde se desea loguear
+LOGDIR=/home/$USER
+LOGDIR+='/'
+
+# Archivo de log
+logWhats=logWhatsapp.txt
+
+# Abre el Home de Whatsapp para despertar el equipo
+adb -s $DEVICE shell am start -n com.whatsapp/com.whatsapp.HomeActivity | touch $LOGDIR$logWhats | ts | tee -a $LOGDIR$logWhats
+
+# Duerme para esperar que el equipo responda
+sleep 7
+
+# Activa la key Home del equipo para minimizar la app (WhatsApp en este caso)
+adb -s $DEVICE shell input keyevent 3

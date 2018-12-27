e36431653f9635ef8232e5fb368afc206c5db621
SpaceHotDog <nicolassolignac@gmail.com>
2018-12-27 15:12:42 -0300

    Stuff

--- deployScript.sh
+++ deployScript.sh
@@ -1,47 +1,57 @@
-# Useful links
-# ------------
-# https://stackoverflow.com/questions/11880070/how-to-run-a-script-as-root-in-jenkins
-# https://wiki.jenkins.io/display/JENKINS/Jenkins+SSH
+#sh '''
 
+#Definimos la ruta de trabajo
+ROOTDIR="/usr/CMP/servicios_cmp_backend"
 
-# TODO: make the word to search in 'grep' as a var as:
-# echo "Tell me the name..."
-# read WORD
-# FILENAME=ls . | grep "$WORD" | cut -d '.' -f 1
-
-# https://shapeshed.com/unix-cut/
-#Store file name for further use
-FILENAME=$(ls . | grep "asd" | cut -d '.' -f 1 | sort -u)
-
-#Define root dir of the project
-ROOTDIR="/home/$USER/testROOT"
-
-#ROOT folder cleaning
+#Limpiamos la carpeta de los binarios obsoletos // TODO
+#rm *.jar
 cd $ROOTDIR
 
-rm *.jar_* | mv *.jar ${FILENAME}.jar_$(date +%d-%m-%Y)
+echo "DONDE ESTOY?"
+echo ${WORKSPACE}
+pwd
+rm *.jar_* || true
 
-# DEPLOY
-touch $FILENAME.jar
+#Guardamos el nombre del archivo de BACKUP
+#FILENAME=$(ls . | grep -i "servicios_cmp_backend" | cut -d '-' -f 1 | sort -u)
+FILENAME=servicios_cmp_backend.jar
+echo $FILENAME
+
+#Realizo el BACKUP
+cp ${FILENAME} ${FILENAME}_$(date +%d-%m-%Y) || true
+
+#DEPLOY
+echo "DEPLOY"
+cp ${WORKSPACE}/target/*.jar $FILENAME && echo "Se realizo el DEPLOY"
 
 #Artifact perms
-chown --reference *.jar_$(date +%d-%m-%Y) $FILENAME.jar
-chmod --reference *.jar_$(date +%d-%m-%Y) $FILENAME.jar
+chown --reference *.jar_$(date +%d-%m-%Y) $FILENAME
+chmod --reference *.jar_$(date +%d-%m-%Y) $FILENAME
 
-# API var
-API="$FILENAME.service"
+#ps aux | grep -v grep | grep $FILENAME
 
-#  RHEL / CentOS 7.x httpd restart command
-RESTART="/bin/systemctl restart $API"
+# Variable API
+API="servicios-cmp-backend"
 
 #Path to pgrep command
 PGREP="/usr/bin/pgrep"
 
-# Find process pid
-$PGREP ${API}
+#Find process pid
+pgrep $(echo $API.jar) || true
 
-if [ $? -ne 0 ] #If process not running
-then
-    # Restart it
-    $RESTART
-fi
+#Control del servicio
+#START="/usr/sbin/service $API start"
+#STOP="/usr/sbin/service $API stop"
+
+##Si el proceso no esta corriendo
+#if [ $? -ne 0 ]
+#then
+#    # Inicio el servicio
+#    /usr/sbin/service $(echo $API) start
+#fi
+
+/usr/sbin/service $API stop || true
+sleep 1 && /usr/sbin/service $API start
+ps aux | grep -v grep | grep $FILENAME
+
+#'''

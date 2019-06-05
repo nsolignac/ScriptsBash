06a0afdb42e564ad7bf01c584cff27dd4932c6f9
SpaceHotDog <nicolassolignac@gmail.com>
2019-06-05 16:40:10 -0300

    Update - deployScript

--- deployScript.sh
+++ deployScript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Definimos la ruta de trabajo
-ROOTDIR="/appuser/multichannel"
+ROOTDIR="/home/t1000/mc-api"
 FILENAME="multichannel"
+ENVIRONMENT="integration"
 
 #Limpiamos la carpeta de los binarios obsoletos
 cd $ROOTDIR
@@ -13,15 +14,15 @@ mv ${FILENAME}-*.jar ${FILENAME}_$(date +%d-%m-%Y).bkp || true
 
 #Agregamos el numero de version al FILENAME
 FILENAME_TMP=$FILENAME
-FILENAME=$FILENAME-$(ls ${WORKSPACE}/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
+FILENAME=$FILENAME-$(ls /home/t1000/mc-api-test/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
 
 #DEPLOY
-echo "DEPLOY"
-cp ${WORKSPACE}/build/libs/*.jar $FILENAME && echo "Se realizo el DEPLOY"
+echo "Comienzo el Deploy"
+cp /home/t1000/mc-api-test/build/libs/*.jar $FILENAME && echo "Se realizo el DEPLOY"
 
 #Permisos
-chown --reference *.jar_$(date +%d-%m-%Y) $FILENAME
-chmod --reference *.jar_$(date +%d-%m-%Y) $FILENAME
+chown --reference *.jar_$(date +%d-%m-%Y) $FILENAME || chown appuser:appuser $FILENAME
+chmod --reference *.jar_$(date +%d-%m-%Y) $FILENAME || chmod 775 $FILENAME
 
 #Reiniciamos el proceso
 pkill -9 -f $FILENAME_TMP

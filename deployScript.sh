b78d9e31c9c5f3da8016252be0185c5f80475694
SpaceHotDog <nicolassolignac@gmail.com>
2019-06-05 11:39:27 -0300

    Added to deployScript - Automatic name resolution

--- deployScript.sh
+++ deployScript.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
-
 #Definimos la ruta de trabajo
 ROOTDIR="/appuser/multichannel"
+FILENAME="multichannel"
 
 #Limpiamos la carpeta de los binarios obsoletos
 cd $ROOTDIR
-rm *.jar_* || true
-rm nohup.bkp || true
-
-#Guardamos el nombre del archivo de BACKUP
-FILENAME=$(ls . | grep -i "multichannel" | cut -d '-' -f 1 | sort -u)
-echo $FILENAME
+rm *.bkp|| true
 
 #Realizo el BACKUP
 mv nohup.out nohup.bkp || true
-cp ${FILENAME}-*.jar ${FILENAME}_$(date +%d-%m-%Y).bkp || true
+mv ${FILENAME}-*.jar ${FILENAME}_$(date +%d-%m-%Y).bkp || true
+
+#GREP FILENAME
+FILENAME=$FILENAME-$(ls ${WORKSPACE}/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
 
 #DEPLOY
 echo "DEPLOY"
-FILENAME=$FILENAME.jar
 cp ${WORKSPACE}/build/libs/*.jar $FILENAME && echo "Se realizo el DEPLOY"
 
 #Permisos
 chown --reference *.jar_$(date +%d-%m-%Y) $FILENAME
 chmod --reference *.jar_$(date +%d-%m-%Y) $FILENAME
 
-#Path to pgrep command
-PGREP="/usr/bin/pgrep"
-
-#Find process pid
-pgrep $(echo $FILENAME) || true
-ps aux | grep -v grep | grep $FILENAME
+#Reiniciamos el proceso
+pkill -9 -f $FILENAME
+nohup java -jar -Dgrails.env=$ENVIRONMENT -Dserver.port=443 $FILENAME &

082151b1d1509c2ee4f4bff026a46d0e3af56afd
SpaceHotDog <nicolassolignac@gmail.com>
2019-06-07 14:03:51 -0300

    Update deployScript - The whole thing

--- deployScript.sh
+++ deployScript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Definimos la ruta de trabajo
-ROOTDIR="/home/t1000/mc-api"
+ROOTDIR="/appuser/multichannel"
 FILENAME="multichannel"
-ENVIRONMENT="integration"
 
 #Limpiamos la carpeta de los binarios obsoletos
 cd $ROOTDIR
@@ -14,11 +13,11 @@ mv ${FILENAME}-*.jar ${FILENAME}_$(date +%d%m%Y).bkp || true
 
 #Agregamos el numero de version al FILENAME
 FILENAME_TMP=$FILENAME
-FILENAME=$FILENAME-$(ls /home/t1000/mc-api-test/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
+FILENAME=$FILENAME-$(ls ${WORKSPACE}/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
 
 #DEPLOY
 echo "Comienzo el Deploy"
-cp /home/t1000/mc-api-test/build/libs/*.jar $FILENAME && echo "Se realizo el DEPLOY"
+cp ${WORKSPACE}/build/libs/*.jar $FILENAME && echo "Se realizo el DEPLOY"
 
 #Permisos
 chown --reference *.jar_$(date +%d%m%Y) $FILENAME || chown appuser:appuser $FILENAME

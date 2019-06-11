0124bb5072b85e1187b457e9acf77e31c3438e11
SpaceHotDog <nicolassolignac@gmail.com>
2019-06-11 17:46:02 -0300

    Added - deployScript FULL

--- deployScript.sh
+++ deployScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Definimos la ruta de trabajo
+#Definimos algunas variables
 ROOTDIR="/appuser/multichannel"
 FILENAME="multichannel"
 
@@ -11,7 +11,6 @@ rm *.bkp|| true
 mv nohup.out nohup.bkp || true
 mv ${FILENAME}-*.jar ${FILENAME}_$(date +%d%m%Y).bkp || true
 
-#Agregamos el numero de version al FILENAME
 FILENAME_TMP=$FILENAME
 FILENAME=$FILENAME-$(ls ${WORKSPACE}/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
 
@@ -20,9 +19,15 @@ echo "Comienzo el Deploy"
 cp ${WORKSPACE}/build/libs/*.jar $FILENAME && echo "Se realizo el DEPLOY"
 
 #Permisos
-chown --reference *.jar_$(date +%d%m%Y) $FILENAME || chown appuser:appuser $FILENAME
-chmod --reference *.jar_$(date +%d%m%Y) $FILENAME || chmod 775 $FILENAME
+chown --reference *_$(date +%d%m%Y).bkp $FILENAME || chown appuser:appuser $FILENAME
+chmod --reference *_$(date +%d%m%Y).bkp $FILENAME || chmod 775 $FILENAME
 
 #Reiniciamos el proceso
-pkill -9 -f $FILENAME_TMP
+MyPID=$(pgrep -f "java -jar -Dgrails.env=$ENVIRONMENT -Dserver.port=443 $FILENAME_TMP")
+kill -9 $MyPID
+
+#Evitamos que Jenkins mate el servicio
+JENKINS_NODE_COOKIE="DontKillMe"
+
+echo "Iniciamos el servicio: $FILENAME"
 nohup java -jar -Dgrails.env=$ENVIRONMENT -Dserver.port=443 $FILENAME &

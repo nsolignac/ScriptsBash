83ac91a1bdc445500eabfce192757835b12f1c8d
SpaceHotDog <nicolassolignac@gmail.com>
2019-06-19 12:02:49 -0300

    Modified - deployScript to Topic Service MC

--- deployScript.sh
+++ deployScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Definimos algunas variables
-ROOTDIR="/appuser/multichannel"
-FILENAME="multichannel"
+ROOTDIR="/appuser/topic-service"
+FILENAME="topic-service"
 
 #Limpiamos la carpeta de los binarios obsoletos
 cd $ROOTDIR
@@ -12,7 +12,7 @@ mv nohup.out nohup.bkp || true
 mv ${FILENAME}-*.jar ${FILENAME}_$(date +%d%m%Y).bkp || true
 
 FILENAME_TMP=$FILENAME
-FILENAME=$FILENAME-$(ls ${WORKSPACE}/build/libs/ | grep 'api-[0-3]\.[0-9][0-9].jar$' | grep -o '[0-3]\.[0-9][0-9]').jar
+FILENAME=$FILENAME-$(ls ${WORKSPACE}/build/libs/ | grep 'topic-service-release*i*' | grep -o '[0-3]\.[0-9][0-9].[0-9]').jar
 
 #DEPLOY
 echo "Comienzo el Deploy"
@@ -23,11 +23,11 @@ chown --reference *_$(date +%d%m%Y).bkp $FILENAME || chown appuser:appuser $FILE
 chmod --reference *_$(date +%d%m%Y).bkp $FILENAME || chmod 775 $FILENAME
 
 #Reiniciamos el proceso
-MyPID=$(pgrep -f "java -jar -Dgrails.env=$ENVIRONMENT -Dserver.port=443 $FILENAME_TMP")
+MyPID=$(pgrep -f "java -jar $FILENAME_TMP")
 kill -9 $MyPID
 
 #Evitamos que Jenkins mate el servicio
-JENKINS_NODE_COOKIE="DontKillMe"
+JENKINS_NODE_COOKIE="JustDontKillMe"
 
 echo "Iniciamos el servicio: $FILENAME"
-nohup java -jar -Dgrails.env=$ENVIRONMENT -Dserver.port=443 $FILENAME &
+nohup java -jar $FILENAME &

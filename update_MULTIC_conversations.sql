7b8ac2e2438d49fea31e73e7920620191ca8f185
SpaceHotDog <nicolassolignac@gmail.com>
2019-07-15 13:31:21 -0300

    Added 3 - Db's Sondeos Folder Content

---
+++ update_MULTIC_conversations.sql
@@ -0,0 +1,12 @@
+SELECT * FROM multic.conversation
+WHERE company_id = '99' AND messenger_id = '197';
+
+SELECT COUNT(id) FROM multic.conversation
+WHERE company_id = '99' AND messenger_id = '197';
+
+SELECT * FROM multic.conversation
+WHERE company_id = '99' AND messenger_id = '197' AND last_updated >= NOW() - INTERVAL 50 DAY;
+
+UPDATE multic.conversation
+SET status = 'closed'
+WHERE company_id = '99' AND messenger_id = '197' AND last_updated >= NOW() - INTERVAL 50 DAY;

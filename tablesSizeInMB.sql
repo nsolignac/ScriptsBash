bffd2c15a1fe7aa98b2a8baffbde29a11c3c51c1
SpaceHotDog <nicolassolignac@gmail.com>
2019-07-25 12:44:29 -0300

    Modified - tableSizeInMB.sql Comments

--- tablesSizeInMB.sql
+++ tablesSizeInMB.sql
@@ -1,3 +1,7 @@
+/*
+SOURCE: https://medium.com/@vcomposieux/audit-your-mysql-or-mariadb-database-292e710b9e5e
+*/
+
 SELECT table_name, engine, row_format, table_rows, ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "size (mb)"
 FROM information_schema.tables
 WHERE table_schema = "<database>"

379d6658e23d21e2d1b99a342975619210483fff
T800 <t800@t800.localdomain>
2020-11-09 15:29:49 -0300

    1 modified - Added SOURCES comment

--- DockerOverlayLinux.sh
+++ DockerOverlayLinux.sh
@@ -1,4 +1,7 @@
-## SOURCE: https://github.com/moby/moby/issues/29358
+## SOURCES: 
+## https://github.com/moby/moby/issues/29358
+## https://gist.github.com/epcim/cbe1e51b1f8ae011d84ce7a754401398
+
 
 docker inspect $(docker ps -qa) | grep -oE '[a-f0-9]{64}' >> inspect-hashs.txt
 docker inspect $(docker images -qa) | grep -oE '[a-f0-9]{64}' >> inspect-hashs.txt
